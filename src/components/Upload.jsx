04e6370d45119c8ba4fa0f75f04f8a2807ce8567
AjayKarthikBoopathy <ajaykarthikbak97@gmail.com>
2023-12-21 17:28:25 +0530

    upload-style

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -14,7 +14,7 @@ import { useNavigate } from "react-router-dom";
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   background-color: #000000a7;
@@ -44,7 +44,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  position: fixed;
+  position: relative;
 `;
 const Close = styled.div`
   position: absolute;
